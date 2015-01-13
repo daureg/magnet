b9b1fda944c0fef61bc30ae8f35f3764fe04e30a
Géraud Le Falher <daureg@gmail.com>
2015-01-13 18:08:08 +0100

    test on (small) real data

---
+++ veverica/real_world.py
@@ -0,0 +1,159 @@
+#! /usr/bin/env python
+# vim: set fileencoding=utf-8
+"""Read signed network from SNAP file and try to predict sign on small
+subgraph."""
+import random as r
+import convert_experiment as cexp
+import redensify
+G = {}
+EDGE_SIGN = {}
+DEGREES = None
+
+
+def add_neighbor(node, neighbor):
+    """add `neighbor` to adjacency list of `node`"""
+    if node in G:
+        G[node].add(neighbor)
+    else:
+        G[node] = set([neighbor])
+
+
+def add_signed_edge(a, b, sign):
+    """Add a `sign`ed edge between `a` and `b`"""
+    if a > b:
+        a, b = b, a
+    add_neighbor(a, b)
+    add_neighbor(b, a)
+    EDGE_SIGN[(a, b)] = sign
+
+
+def read_original_graph(filename):
+    """Read a signed graph from `filename` and compute its degree sequence"""
+    global DEGREES
+    with open(filename) as source:
+        for line in source:
+            if line.startswith('#'):
+                continue
+            i, j, sign = [int(_) for _ in line.split()]
+            add_signed_edge(i, j, sign > 0)
+    DEGREES = sorted(((node, len(adj)) for node, adj in G.items()),
+                     key=lambda x: x[1])
+
+
+def get_ego_nodes(root, hops=3):
+    """Get all the nodes connected to `root` by a path of length less than
+    `hops`."""
+    res = []
+    border = [root]
+    for step in range(hops):
+        new_border = set()
+        for n in border:
+            res.append(n)
+            for neighbor in G[n]:
+                if neighbor not in res and neighbor not in border:
+                    new_border.add(neighbor)
+        border = new_border
+    return res
+
+
+def create_subgraph(nodes):
+    """Make a graph from all edges induced by `nodes`."""
+    cexp.new_graph()
+    for u, node in enumerate(nodes):
+        redensify.G[u] = set()
+        for neighbor in G[node]:
+            if neighbor not in nodes:
+                continue
+            v = nodes.index(neighbor)
+            edge = (node, neighbor) if node < neighbor else (neighbor, node)
+            cexp.add_signed_edge(u, v, EDGE_SIGN[edge])
+    cexp.finalize_graph()
+
+
+def find_removable_edges():
+    """Return all possible edges that can be removed without creating orphan
+    nodes (but the graph might be disconnected)"""
+    isolated = [node for node, adj in redensify.G.items() if len(adj) <= 1]
+    can_be_removed = []
+    degrees = [len(adj) for node, adj in redensify.G.items()]
+    for i, j in redensify.EDGES_SIGN.keys():
+        if i not in isolated and j not in isolated:
+            can_be_removed.append((i, j))
+            degrees[i] -= 1
+            if degrees[i] <= 1:
+                isolated.append(i)
+            degrees[j] -= 1
+            if degrees[j] <= 1:
+                isolated.append(j)
+    return can_be_removed
+
+
+def generate_subgraph_with_test_set():
+    """Extract a subgraph from the big one and remove some edges, that are to
+    be predicted."""
+    nodes = get_ego_nodes(37224)
+    create_subgraph(nodes)
+    can_be_removed = find_removable_edges()
+    indeed_removed = {}
+    for i, j in can_be_removed:
+        edge = (i, j)
+        if r.random() < .3:
+            indeed_removed[edge] = redensify.EDGES_SIGN[edge]
+            del redensify.EDGES_SIGN[edge]
+            redensify.G[i].remove(j)
+            redensify.G[j].remove(i)
+    cexp.finalize_graph()
+    return indeed_removed
+
+
+
+
+def make_prediction(edges_to_be_predicted, cc_run=150):
+    """Return the true sign of the `edges_to_be_predicted` along with the
+    predicted sign from a clustering."""
+    # find a clustering with the fewest disagreements
+    best = (1e6, None)
+    for i in range(cc_run):
+        cluster = cexp.cc_pivot()
+        cost = sum(cexp.count_disagreements(cluster))
+        if cost < best[0]:
+            best = (cost, cluster)
+
+    # Use it to predict the sign of missing edges
+    cluster = best[1]
+    gold, pred = [], []
+    for (i, j), sign in edges_to_be_predicted.items():
+        pred.append(cluster[i] == cluster[j])
+        gold.append(sign)
+    return gold, pred
+
+
+def binary_score(gold, pred):
+    """Evaluate binary classification result `pred` against `gold` values.
+    For now this is simply accuracy"""
+    return sum((g == p for g, p in zip(gold, pred)))/len(gold)
+
+
+def process_real(kwargs):
+    removed = generate_subgraph_with_test_set()
+    redensify.PIVOT_SELECTION = kwargs['pivot']
+    start = cexp.default_timer()
+    redensify.complete_graph(one_at_a_time=kwargs['one_at_a_time'])
+    elapsed = cexp.default_timer() - start
+    gold, pred = make_prediction(removed)
+    return elapsed, binary_score(gold, pred)
+
+
+def run_real(one_at_a_time, pool=None,
+             pivot=redensify.PivotSelection.Uniform, n_rep=100):
+    args = cexp.repeat({"pivot": pivot, "one_at_a_time": one_at_a_time}, n_rep)
+
+    if pool:
+        runs = list(pool.imap_unordered(process_real, args,
+                                        chunksize=n_rep//cexp.NUM_THREADS))
+    else:
+        runs = [process_real(_) for _ in args]
+    res = {'time': list(map(cexp.itemgetter(0), runs)),
+           'nb_error': list(map(cexp.itemgetter(1), runs))}
+    cexp.p.save_var(cexp.savefile_name('real', [0, 0], pivot, one_at_a_time),
+                    res)

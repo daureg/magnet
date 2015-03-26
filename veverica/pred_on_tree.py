b155c081fcf5f1462f610f325cc696aeded1dd23
Géraud Le Falher <daureg@gmail.com>
2015-03-26 18:58:11 +0100

    implement prediction on larger than tree structure

--- veverica/pred_on_tree.py
+++ veverica/pred_on_tree.py
@@ -24,18 +24,13 @@ class BFSTree(BFSVisitor):
 def read_tree(filename):
     """Read a list of edge and return adjacency list"""
     tree = {}
+    edges = []
     with open(filename) as f:
         for line in f.readlines():
             u, v = [int(_) for _ in line.strip().split(',')]
-            if u in tree:
-                tree[u].add(v)
-            else:
-                tree[u] = set([v])
-            if v in tree:
-                tree[v].add(u)
-            else:
-                tree[v] = set([u])
-    return tree
+            add_edge_to_tree(tree, u, v)
+            edges.append((u, v))
+    return tree, edges
 
 
 def dfs_tagging(tree, edges, root):
@@ -44,6 +39,7 @@ def dfs_tagging(tree, edges, root):
     tags = defaultdict(int)
 
     def _dfs_tagging(node, sign):
+        assert sign in [-1, 1], 'edges sign should not be boolean'
         tags[node] = sign
         for child in tree[node]:
             if tags[child] != 0:
@@ -71,6 +67,18 @@ def make_pred(tree, tags):
     return gold, pred
 
 
+def add_edge_to_tree(tree, u, v):
+    """Update adjacency list `tree` with the (u, v) edge"""
+    if u in tree:
+        tree[u].add(v)
+    else:
+        tree[u] = set([v])
+    if v in tree:
+        tree[v].add(u)
+    else:
+        tree[v] = set([u])
+
+
 def read_in_memory_tree(graph, tree_map):
     """Return adjacency list from a `tree_map`"""
     tree = {}
@@ -78,14 +86,7 @@ def read_in_memory_tree(graph, tree_map):
         if not tree_map[e]:
             continue
         u, v = int(e.source()), int(e.target())
-        if u in tree:
-            tree[u].add(v)
-        else:
-            tree[u] = set([v])
-        if v in tree:
-            tree[v].add(u)
-        else:
-            tree[v] = set([u])
+        add_edge_to_tree(tree, u, v)
     return tree
 
 
@@ -111,6 +112,162 @@ def get_bfs_tree(G, root):
                 e = v, w if v < w else w, v
                 tree.append(e)
     return tree
+
+def read_spanner_from_file(basename):
+    """Reparse information written by galaxy.export_spanner"""
+    adjacency, spanner = read_tree(basename+'.edges')
+    with open(basename+'.sm') as f:
+        raw_star_membership = f.readlines()
+    star_membership = {}
+    for line in raw_star_membership:
+        orig_node, star_idx = [int(_) for _ in line.split()]
+        star_membership[orig_node] = star_idx
+    with open(basename+'.lle') as f:
+        raw_lle = f.readlines()
+    low_level_edges = {}
+    for line in raw_lle:
+        top_edge, orig_edge = line.split('\t')
+        te0, te1 = [int(_) for _ in top_edge.split(', ')]
+        te0, te1 = (te0, te1) if te0 < te1 else (te1, te0)
+        oe0, oe1 = [int(_) for _ in orig_edge.split(', ')]
+        low_level_edges[(te0, te1)] = (oe0, oe1)
+    return spanner, star_membership, low_level_edges, adjacency
+
+
+def split_into_trees(spanner, star_membership):
+    """Build a tree adjacency for each star based on the edges in `spanner`"""
+    trees = defaultdict(dict)
+    for u, v in spanner:
+        head_star = star_membership[u]
+        tail_star = star_membership[v]
+        if head_star == tail_star:
+            add_edge_to_tree(trees[head_star], u, v)
+    return trees
+
+
+def parity_in_star(star_idx, i, j, tags, star_membership):
+    """Return the parity of the i->j path within star numbered `star_idx`"""
+    err = (i, star_membership[i], star_idx, j, star_membership[j])
+    assert star_membership[i] == star_idx == star_membership[j], err
+    return tags[star_idx][i] * tags[star_idx][j]
+
+
+def orient_path(stars_path, low_level_edges, star_membership):
+    """Given a connected list of stars, return a list of edges in the original
+    graph, each oriented from one star to the next one in the path."""
+    def orient_edge(edge, start):
+        """return `edge` with first vertex belonging to `start` star"""
+        if star_membership[edge[0]] == start:
+            return edge
+        return (edge[1], edge[0])
+
+    new_path = []
+    for i, j in zip(stars_path, stars_path[1:]):
+        real_edge = low_level_edges[(i, j) if i < j else (j, i)]
+        new_path.append(orient_edge(real_edge, i))
+    return new_path
+
+
+def parity(i, j, spanner, edge_sign, tags, star_membership, low_level_edges):
+    """Return the parity of the i->j path"""
+    i_star = star_membership[i]
+    j_star = star_membership[j]
+    if i_star == j_star:
+        return parity_in_star(i_star, i, j, tags, star_membership)
+    if i_star > j_star:
+        i, j = j, i
+        i_star, j_star = j_star, i_star
+    star_path = orient_path(shortest_path(spanner, i_star, j_star),
+                            low_level_edges, star_membership)
+    # print(i, j, i_star, j_star, star_path)
+    # TODO actually I could also cache the parity computation between i_star
+    # and j_star
+    parity = parity_in_star(i_star, i, star_path[0][0], tags, star_membership)
+    for edge, next_edge in zip(star_path, star_path[1:]):
+        u, v = edge
+        canonical_edge = (u, v) if u < v else (v, u)
+        parity *= edge_sign[canonical_edge]
+        parity *= parity_in_star(star_membership[edge[1]],
+                                 edge[1], next_edge[0])
+    u, v = star_path[-1]
+    canonical_edge = (u, v) if u < v else (v, u)
+    parity *= edge_sign[canonical_edge]
+    parity *= parity_in_star(j_star, star_path[-1][1], j, tags,
+                             star_membership)
+    return parity
+
+
+def brute_parity(i, j, low_level_graph, edge_sign):
+    """Compute directly the shortest_path between i and j in low_level_graph
+    and multiply edges sign"""
+    parity = 1
+    path = shortest_path(low_level_graph, i, j)
+    for u, v in zip(path, path[1:]):
+        parity *= edge_sign[(u, v) if u < v else (v, u)]
+    return parity
+
+
+def shortest_path(graph, src, dst):#vertices):
+    """Return a shortest_path between vertices[0] and vertices[1]"""
+    # src, dst = vertices
+    q = deque()
+    discovered = [False for _ in range(len(graph))]
+    q.append(src)
+    discovered[src] = True
+    predecessor = {}
+    while q:
+        v = q.popleft()
+        for w in graph[v]:
+            if not discovered[w]:
+                predecessor[w] = v
+                q.append(w)
+                discovered[w] = True
+    path = [dst]
+    next_step = predecessor[dst]
+    while next_step != src:
+        path.append(predecessor[path[-1]])
+        next_step = predecessor[path[-1]]
+    path.append(src)
+    # TODO compute parity here (in reversed order, doesn't matter) and return
+    # only [path[-1], path[-2], path[1], path[0]] (special case if there is
+    # only one edge (ie two vertices)) plus the parity of the path in the
+    # middle (or 1 if nothing)
+    return list(reversed(path))
+
+
+def predict_edges(basename):
+    import redensify
+    edge_signs = {}
+    for e, s in redensify.EDGES_SIGN.items():
+        edge_signs[e] = 1 if s else -1
+    _ = read_spanner_from_file(basename)
+    spanner, star_membership, low_level_edges, low_level_graph = _
+    top_graph = {}
+    for u, v in low_level_edges.keys():
+        add_edge_to_tree(top_graph, u, v)
+    test_edges = set(edge_signs.keys()) - set(spanner)
+    trees = split_into_trees(spanner, star_membership)
+    tags = {}
+    for star, tree in trees.items():
+        tags[star] = dfs_tagging(tree, edge_signs,
+                                 root=next(iter(tree.keys())))
+    # Since we build trees and tags based on edges of the span, stars
+    # consisting of a single node do not appear. Thus we create their tag
+    # structure manually
+    for node in top_graph.keys():
+        if node not in tags:
+            tags[node] = defaultdict(lambda : 1)
+    gold, pred, brute_pred = [], [], []
+    for e in test_edges:
+        s = edge_signs[e]
+        u, v = e
+        gold.append(s)
+        uv_sign = parity(u, v, top_graph, edge_signs, tags, star_membership,
+                         low_level_edges)
+        pred.append(uv_sign)
+        brute_pred.append(brute_parity(u, v, low_level_graph, edge_signs))
+    return gold, pred, brute_pred
+
 if __name__ == '__main__':
     # pylint: disable=C0103
     import sys
@@ -140,7 +297,7 @@ if __name__ == '__main__':
     lcc = label_largest_component(k)
     k.set_vertex_filter(lcc)
     print_diag('Extract largest component')
-    tree = read_tree(tree_edges)
+    tree, _ = read_tree(tree_edges)
     print_diag('Read galaxy tree')
     f1, mc = assess_tree_fitness(tree)
     print_diag('Predict with galaxy tree')
@@ -157,7 +314,7 @@ if __name__ == '__main__':
         bfst = get_bfs_tree(rw.G, root)
         with open('__.dat', 'w') as f:
             f.write('\n'.join(('{}, {}'.format(*e) for e in bfst)))
-        tree = read_tree('__.dat')
+        tree, _ = read_tree('__.dat')
         f1, mc = assess_tree_fitness(tree)
         print_diag('Predict with BFS {}'.format(root))
         print('{}{:.3f}'.format('F1-score'.ljust(60), f1))

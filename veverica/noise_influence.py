82354acfdbdd117eac1cb961e702e1cba1b9eb77
Géraud Le Falher <daureg@gmail.com>
2015-04-10 11:55:09 +0200

    test noisy preferential attachment model

--- veverica/noise_influence.py
+++ veverica/noise_influence.py
@@ -9,36 +9,61 @@ import graph_tool
 import os
 import pred_on_tree as pot
 # import cc_pivot as cc
-ROOTS = [12, 34, 39, 42, 43, 47, 95, 128, 136, 169, 170, 209, 225, 251, 280,
-         280, 317, 349, 351, 369, 399, 406, 433, 458, 476, 484, 498, 499, 503,
-         506, 543, 553, 561, 565, 579, 621, 666, 676, 687, 739, 789, 795, 843,
-         847, 854, 875, 883, 907, 928, 975]
+PA = True
+ROOTS_ER = [12, 34, 39, 42, 43, 47, 95, 128, 136, 169, 170, 209, 225, 251, 280,
+            280, 317, 349, 351, 369, 399, 406, 433, 458, 476, 484, 498, 499,
+            503, 506, 543, 553, 561, 565, 579, 621, 666, 676, 687, 739, 789,
+            795, 843, 847, 854, 875, 883, 907, 928, 975]
+ROOTS_PA = [71, 94, 76, 89, 113, 93, 41, 75, 157, 35, 95, 56, 36, 45, 25, 58,
+            38, 53, 69, 79, 21, 33, 52, 43, 34, 26, 46, 47, 66, 31, 40, 28, 7,
+            32, 29, 30, 22, 27, 39, 18, 17, 24, 5, 23, 20, 15, 16, 19, 14, 13]
+BASENAME = 'universe/noise'
+NUM_TRAIN_EDGES = 0
+if PA:
+    BASENAME += 'PA'
+    ROOTS = ROOTS_PA
+    K = 2
+else:
+    ROOTS = ROOTS_ER
+    K = 3
 
 
 def get_graph():
-    if os.path.isfile('universe/noise.gt'):
-        g = graph_tool.load_graph('universe/noise.gt')
-        dst_mat = np.load('universe/noise_dst.npy')
+    if os.path.isfile(BASENAME+'.gt'):
+        g = graph_tool.load_graph(BASENAME+'.gt')
+        dst_mat = np.load(BASENAME+'_dst.npy')
         cexp.to_python_graph(g)
         return g, dst_mat
-    cexp.random_signed_communities(2, 500, 13, 11.5/500, .0, .0)
-    g = cexp.to_graph_tool()
+    if not PA:
+        cexp.random_signed_communities(2, 500, 13, 11.5/500, .0, .0)
+        g = cexp.to_graph_tool()
+    else:
+        cexp.preferential_attachment(1000, gamma=1.4, m=12)
+        cexp.turn_into_signed_graph_by_propagation(2)
+        DEGREES = sorted(((node, len(adj))
+                          for node, adj in cexp.redensify.G.items()),
+                         key=lambda x: x[1])
+        u, v = DEGREES[-1][0], DEGREES[-2][0]
+        u, v = v, u if u > v else u, v
+        del cexp.redensify.EDGES_SIGN[(u, v)]
+        cexp.redensify.G[u].remove(v)
+        cexp.redensify.G[v].remove(u)
     n = g.num_vertices()
     dst = shortest_distance(g, dense=False)
     dst_mat = np.zeros((n, n), dtype=np.uint8)
     for v in g.vertices():
         dst_mat[int(v), :] = dst[v].a.astype(np.uint8)
-    g.save('universe/noise.gt')
-    np.save('universe/noise_dst', dst_mat)
+    g.save(BASENAME+'.gt')
+    np.save(BASENAME+'_dst', dst_mat)
 
 
 def compute_trees(seed=None):
     if seed:
         import galaxy
         galaxy.galaxy_maker_clean(redensify.G, 5,
-                                  outname='universe/noise_{}'.format(seed))
+                                  outname=BASENAME+'_{}'.format(seed))
         return None
-    assert len(redensify.G.keys()) == 1000
+    assert len(redensify.G.keys()) == 1000, len(redensify.G.keys())
     trees = []
     for root in ROOTS:
         bfs_edges = pot.get_bfs_tree(redensify.G, root)
@@ -129,7 +154,7 @@ if __name__ == '__main__':
     #                   for _ in zip(np.mean(bfs_stretch, 0),
     #                                np.std(bfs_stretch, 0))]))
     # for k in range(3):
-    #     basename = 'universe/noise_' + str(k)
+    #     basename = BASENAME+'_' + str(k)
     #     spanner_edges, _, _, _ = pot.read_spanner_from_file(basename)
     #     res = compute_stretch(gt_graph, dst_mat, spanner_edges)
     #     print(' & '.join(['{:.3f}'.format(m) for m in res]))

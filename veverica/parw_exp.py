4465745bfac227e92de852156bb112d8f7fe52f3
Géraud Le Falher <daureg@gmail.com>
2015-05-21 13:58:40 +0200

    expe on good old datasets

--- veverica/parw_exp.py
+++ veverica/parw_exp.py
@@ -7,11 +7,15 @@ if __name__ == '__main__':
     # pylint: disable=C0103
     num_exp = 4
     f = 'belgrade/gplus.my'
-    G, E = scratch.load_graph(f)
-    for noise in [0, 1, 2, 5, 10]:
+    osn = ['belgrade/{}.my'.format(_)
+           for _ in ['wiki', 'slash', 'epi']]
+    for f in osn:
+        G, E = scratch.load_graph(f)
+    # for noise in [0, 1, 2, 5, 10]:
+        noise = 0
         Enoisy = scratch.add_noise(E, noise)
         for _ in range(num_exp):
             start = clock()
             name = '{}_{}_{}'.format(f, noise, _)
-            scratch.process_graph(G, Enoisy, noise, name, asym=True)
+            scratch.process_graph(G, Enoisy, noise, name, asym=False)
             print('{}{:.1f} secs'.format(name.ljust(60), clock() - start))

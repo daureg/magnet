623e8e699d0bcbee21d8c95a3b4e8aedbf6f96be
Géraud Le Falher <daureg@gmail.com>
2015-09-24 19:04:00 +0200

    synthetic generation and optimization of contextual clustering

---
+++ veverica/synth.py
@@ -0,0 +1,231 @@
+# vim: set fileencoding=utf-8
+# pylint: disable=no-member
+from collections import Counter
+from grid_stretch import add_edge
+from heap.heap import heap
+from itertools import combinations, product
+from sklearn.cluster import SpectralClustering
+from sklearn.metrics import mean_squared_error, f1_score
+from warnings import warn
+import baseline as bl
+import cvxpy as cvx
+import numpy as np
+
+
+def create_graph(users, xs):
+    # Another way of doing, which assign optimal direction to each edges
+    within_size, across_size_ = 4.0, 1.0
+    n = users.shape[0]
+    C = np.dot(users, xs.T)
+    heads, tails = zip(*combinations(range(n), 2))
+    heads = np.array(heads)
+    tails = np.array(tails)
+    D = np.abs(C[heads,:] - C[tails,:])
+    best_dir = np.argmin(D, 1).astype(int)
+    final_E = {}
+    sc = SpectralClustering(n_clusters=4, affinity='precomputed', n_init=20)
+    for dir_id in range(xs.shape[0]):
+        t = np.percentile(D[best_dir == dir_id, dir_id], 50)
+        mask = np.logical_and(best_dir == dir_id, D[:, dir_id]<t)
+        edges_idx = np.where(mask)[0]
+        node_in_dir = set(tails[edges_idx]).union(set(heads[edges_idx]))
+        this_dir_to_nodes = {i: v for i, v in enumerate(sorted(node_in_dir))}
+        nodes_to_this_dir = {v: k for k, v in this_dir_to_nodes.items()}
+
+        dn = len(node_in_dir)
+        A, G, E = np.zeros((dn, dn)), {}, set()
+        for i, u, v in zip(edges_idx, heads[edges_idx], tails[edges_idx]):
+            u, v = nodes_to_this_dir[u], nodes_to_this_dir[v]
+            if u > v:
+                u, v = v, u
+            add_edge(G, u, v)
+            E.add((u, v))
+            A[u, v] = D[i, dir_id]
+            A[v, u] = D[i, dir_id]
+        sc.fit(A)
+        labels = sc.labels_
+        cluster_sizes = Counter(labels)
+        inner_target = {c: within_size*s for c, s in cluster_sizes.items()}
+        across_target = {(a, b):
+                         across_size_*(cluster_sizes[a]+cluster_sizes[a])/2
+                         for a, b in list(combinations(cluster_sizes, 2))}
+        smallest_edges = sorted(edges_idx, key=lambda i: D[i, dir_id])
+
+        nG, nE = {}, set()
+        inner_size = {c: 0 for c in inner_target}
+        across_size = {c: 0 for c in across_target}
+        for i, u, v in zip(smallest_edges,
+                           heads[smallest_edges], tails[smallest_edges]):
+            u, v = nodes_to_this_dir[u], nodes_to_this_dir[v]
+            if u > v:
+                u, v = v, u
+            cu, cv = labels[u], labels[v]
+            add_within = cu==cv and inner_size[cu] < inner_target[cu]
+            pair = (cu, cv) if cu < cv else (cv, cu)
+            add_across = cu != cv and across_size[pair] < across_target[pair]
+            if add_within or add_across:
+                add_edge(nG, u, v)
+                nE.add((u, v))
+                if add_within:
+                    inner_size[cu] += 1
+                else:
+                    across_size[pair] += 1
+            done_within = all((v >= inner_target[k]
+                               for k, v in inner_size.items()))
+            done_across = all((v >= across_target[k]
+                               for k, v in across_size.items()))
+            if done_within and done_across:
+                break
+
+        final_E.update({(this_dir_to_nodes[u], this_dir_to_nodes[v]): dir_id
+                        for u, v in nE})
+    return final_E
+
+
+def to_graph_tool_simple(orig):
+    """transform a adjacency dict into a graph tool structure, suitable for
+    cc_pivot.draw_clustering"""
+    import graph_tool as gt
+    graph = gt.Graph(directed=False)
+    mapping = {v: i for i, v in enumerate(G.keys())}
+    graph.add_vertex(len(mapping))
+    for u, adj in orig.items():
+        u = mapping[u]
+        for v in adj:
+            v = mapping[v]
+            if u < v:
+                graph.add_edge(u, v)
+    return graph, mapping
+
+
+def generate_random_data(n=300, d=4, nx=6):
+    """There are $n$ nodes, each associated with a $d$ dimensional unit norm
+    feature vectors. In addition we pick $nx$ directions at random in
+    $\mathbb{R}^{+d}$."""
+    def norm_rand_matrix(rows, columns):
+        _ = 2*np.random.random((rows, columns))-1
+        return _ / np.sqrt((_ ** 2).sum(-1))[..., np.newaxis]
+    users = norm_rand_matrix(n, d)
+    xs = np.abs(norm_rand_matrix(nx, d))
+    return users, xs
+
+
+def laplacian_from_edges(E, n, nodes_to_idx=None, nb_dir=None):
+    """Compute the `nb_dir` Laplacians of `E`"""
+    nb_dir = nb_dir or len(set(E.values()))
+    L = np.zeros((nb_dir, n, n), dtype=int)
+    for (u, v), k in E.items():
+        if nodes_to_idx:
+            u, v = nodes_to_idx[u], nodes_to_idx[v]
+        L[k][u][u] += 1
+        L[k][v][v] += 1
+        L[k][u][v] = -1
+        L[k][v][u] = -1
+    return L
+
+
+def compute_loss(users, xs, L):
+    nx = xs.shape[0]
+    weight = np.dot(np.ones(nx), np.dot(xs, xs.T))
+    lap = np.zeros(nx)
+    for m, xm in enumerate(xs):
+        vm = np.dot(users, xm[:, np.newaxis])
+        lap[m] = np.dot(vm.T, np.dot(L[m], vm)).ravel()[0]
+    return np.dot(lap, weight)
+
+
+def setup_constraints(users, vis, noise_level=0.0):
+    """Fix the value of visible users and return variable to optimize,
+    potentially adding some noise to them"""
+    n, d = users.shape
+    Ux = cvx.Variable(n*d)
+    constraints = []
+    for u, visible in vis.items():
+        if visible:
+            real_user = np.copy(users[u, :])
+            if noise_level > 0:
+                noise = np.random.normal(scale=noise_level, size=d)
+                real_user += noise
+                real_user /= np.linalg.norm(real_user)
+            constraints.append(Ux[u*d:(u+1)*d] == real_user)
+    return Ux, constraints
+
+
+def setup_optim(users, xs, L, Ux, constraints):
+    """Compute all terms needed to define the optimization problem"""
+    (n, d), nx = users.shape, xs.shape[0]
+    weight = np.dot(np.ones(nx), np.dot(xs, xs.T))
+    laps = []
+    for m, xm in enumerate(xs):
+        k = np.matrix(np.kron(np.eye(n), xm[:, np.newaxis]))
+        vm = k.T*Ux
+        laps.append(cvx.quad_form(vm, L[m]))
+    obj = cvx.Minimize(sum((w*l for w, l in zip(weight, laps))))
+    return cvx.Problem(obj, constraints)
+
+
+def solve_optim(prob, Ux, verbose=True, normalize=True):
+    """return the solution of the problem"""
+    d = prob.constraints[0].size[0]
+    n = Ux.size[0]//d
+    loss = prob.solve(verbose=verbose, solver=cvx.SCS)
+    uval = np.array(Ux.value).ravel()
+    urecovered = uval.reshape(n, d)
+    if normalize:
+        urecovered /= np.sqrt((urecovered ** 2).sum(-1))[..., np.newaxis]
+        if verbose:
+            warn('you need to compute the loss yourself')
+    return urecovered, loss
+
+
+def evaluate_solution(users, urecovered, observed_index, xs=None, E=None,
+                      hidden_edges=None):
+    """Evaluate the quality of the recovered user profile"""
+    mse = mean_squared_error(users[observed_index, :],
+                             urecovered[observed_index, :])
+    if hidden_edges is None or len(hidden_edges) < 1:
+        return mse, None
+    gold = np.array([E[e] for e in sorted(hidden_edges)])
+    eh = sorted(hidden_edges)
+    heads, tails = zip(*eh)
+    Cr = np.dot(urecovered, xs.T)
+    Dr = np.abs(Cr[heads, :] - Cr[tails, :])
+    pred = np.argmin(Dr, 1).astype(int)
+    return mse, f1_score(gold, pred, average='weighted')
+
+
+def hide_edges(G, E, vis, fraction=.1):
+    """hide some edges involving hidden node without deconnecting the graph"""
+    degrees = {u: len(adj) for u, adj in G.items()}
+    h_degrees = heap({u: -len(adj) for u, adj in G.items()
+                      if not vis[u]})
+    edges_hidden = set()
+    num_edges_to_hid = int(fraction*len(E))
+    while h_degrees and len(edges_hidden) < num_edges_to_hid:
+        u = h_degrees.pop()
+        neighbors = sorted((v for v in G[u]
+                            if degrees[v] > max(2, .7*len(G[v]))),
+                           key=degrees.__getitem__, reverse=True)
+        for v in neighbors:
+            degrees[u] -= 1
+            degrees[v] -= 1
+            if v in h_degrees:
+                h_degrees[v] += 1
+            edges_hidden.add((u, v) if u < v else (v, u))
+            if len(edges_hidden) >= num_edges_to_hid:
+                break
+    return edges_hidden
+
+if __name__ == '__main__':
+    # We build the adjacency list considering edges from all directions
+    G={}
+    for u, v in E:
+        add_edge(G, u, v)
+    # Then hide some users, keeping visible a dominating set of the graph so that
+    # every node has a visible neighbors and thus is not trivially 0
+    dms = bl.greedy_dominating_set(G, n)
+    uo, vis = bl.hide_some_users(G, n, fraction_of_hidden=.2, visible_seed=dms)
+
+    E = create_graph(users, xs)
+    L = laplacian_from_edges(E, n)
+    L = laplacian_from_edges({e:k for e,k in E.items() if k not in edges_hidden}, n)

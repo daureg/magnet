abd06ad4215843f19505038b49a1e559e7bb7a45
Géraud Le Falher <daureg@gmail.com>
2016-03-08 17:17:09 +0100

    add self selection for beta and lambda parameters

--- veverica/rank_nodes.py
+++ veverica/rank_nodes.py
@@ -9,29 +9,34 @@ In Proceedings of the Ninth ACM International Conference on Web Search and Data
 Mining (WSDM '16). pp 447-456. http://dx.doi.org/10.1145/2835776.2835816.
 """
 from collections import defaultdict
+import random
 import warnings
-warnings.filterwarnings('ignore', r'invalid value encountered in true_divide',
-                        RuntimeWarning)
 from sklearn.base import BaseEstimator, TransformerMixin
 import numpy as np
+warnings.filterwarnings('ignore', r'invalid value encountered in true_divide',
+                        RuntimeWarning)
 
 
 class NodesRanker(BaseEstimator, TransformerMixin):
 
     """Assign a rank to each node based on its trustworthiness."""
 
-    def __init__(self, beta=0.5, lambda_1=1, tol=1e-11, n_iters=200):
+    def __init__(self, beta=0.5, lambda_1=1, tol=1e-11, n_iters=200,
+                 autotune_budget=0):
         """Initialize the parameters.
 
         beta: default value of trustworthiness
         lambda_1: edge weight multiplier
         tol: no update when change get smaller than tol
         n_iters: maximum number of iteration
+        autotune_budget: number of couples (β, λ) to try by cross-validation on
+        the train set
         """
         self.beta = beta
         self.lambda_1 = lambda_1
         self.tol = tol
         self.n_iters = n_iters
+        self.autotune_budget = autotune_budget
 
     def fit(self, E, N):
         """compute nodes ranking, reputation and optimism.
@@ -44,6 +49,32 @@ class NodesRanker(BaseEstimator, TransformerMixin):
         # adjacency matrix and should be extracted out of it.
         self.Etrain = E
         self.N = N
+        Esplit, params = [], []
+        if self.autotune_budget > 0:
+            Esplit = [NodesRanker.split_edges(E, .8) for _ in range(5)]
+            params = [(random.uniform(0, 1), random.uniform(.1, 2))
+                      for _ in range(self.autotune_budget)]
+            params = [(b, l) for b in np.linspace(.1, .9, 7)
+                      for l in np.linspace(.3, 1.1, 3)]
+        perfs = []
+        logreg = SGDClassifier(loss='log', n_iter=2, class_weight={0: 1.4, 1: 1},
+                               warm_start=True, average=True)
+        for beta, lambda_1 in params:
+            self.beta, self.lambda_1 = beta, lambda_1
+            perf = 0
+            for split in Esplit:
+                self.Etrain, Etest = split
+                self._compute_rpi()
+                self._compute_rep_opt()
+                Xtrain, ytrain = self.transform(self.Etrain)
+                Xtest, ytest = self.transform(Etest)
+                logreg.fit(Xtrain, ytrain)
+                perf += np.sum(logreg.predict(Xtest) == ytest)
+            perfs.append(perf)
+        if perfs:
+            self.beta, self.lambda_1 = params[np.argmax(perfs)]
+            print(self.beta, self.lambda_1)
+        self.Etrain = E
         self._compute_rpi()
         self._compute_rep_opt()
 
@@ -101,38 +132,43 @@ class NodesRanker(BaseEstimator, TransformerMixin):
 
     def _format_feature(self, E):
         X, y = np.zeros((len(E), 4)), np.zeros(len(E), dtype='int8')
-        for i, ((u, v), pos) in enumerate(sorted(E.items())):
+        for i, ((u, v), pos) in enumerate(E.items()):
             X[i, :] = [self.reputation[u], self.reputation[v],
                        self.optimism[u], self.optimism[v]]
             y[i] = int((pos+1)//2)
         return X, y
+
+    @staticmethod
+    def split_edges(E, train_fraction):
+        """Split `E` in (train_fraction, 1-train_fraction) parts."""
+        Etrain, Etest = {}, {}
+        for e, s in E.items():
+            if random.random() < train_fraction:
+                Etrain[e] = 1 if s > 0 else -1
+            else:
+                Etest[e] = 1 if s > 0 else -1
+        return (Etrain, Etest)
 
 if __name__ == '__main__':
     # pylint: disable=C0103
     from sklearn.linear_model import SGDClassifier
     from sklearn.metrics import confusion_matrix, accuracy_score, matthews_corrcoef, f1_score
     import pack_graph as pg
-    import random
     import sys
     from timeit import default_timer as clock
     if len(sys.argv) == 1:
-        datafile = 'directed_wik.pack' 
+        datafile = 'directed_wik.pack'
     else:
         datafile = sys.argv[1]
-    G, E = pg.load_directed_signed_graph('directed_wik.pack')
+    G, E = pg.load_directed_signed_graph(datafile)
     logreg = SGDClassifier(loss='log', n_iter=5, class_weight={0: 1.4, 1: 1},
                            warm_start=True, average=True)
-    nrk = NodesRanker()
+    nrk = NodesRanker(autotune_budget=0)
 
     k = 5
     res = np.zeros((k, 5))
     for i in range(k):
-        Etrain, Etest = {}, {}
-        for e, s in E.items():
-            if random.random() < .9:
-                Etrain[e] = 1 if s else -1
-            else:
-                Etest[e] = 1 if s else -1
+        Etrain, Etest = NodesRanker.split_edges(E, .9)
 
         start = clock()
         nrk.fit(Etrain, len(G))

f72cb53375889f6f3a46982ee115dc8058f91bb4
Géraud Le Falher <daureg@gmail.com>
2016-03-12 12:37:05 +0100

    add additional dataset

--- veverica/exp_tworules.py
+++ veverica/exp_tworules.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("data", help="Which data to use",
-                        choices={'wik', 'sla', 'epi'})
+                        choices={'wik', 'sla', 'epi', 'kiw'})
     # parser.add_argument("-b", "--balanced", action='store_true',
     #                     help="Should there be 50/50 +/- edges")
     # parser.add_argument("-a", "--active", action='store_true',

17a3ba17a9d017bf52cd13efb00bb47ed427b540
Géraud Le Falher <daureg@gmail.com>
2016-05-05 20:38:48 +0200

    typo

--- veverica/runexp.sh
+++ veverica/runexp.sh
@@ -1,4 +1,4 @@
-time p -O -W ignore exp_tworules.py wik -n3
-time p -O -W ignore exp_tworules.py sla -n3
-time p -O -W ignore exp_tworules.py epi -n3
-time p -O -W ignore exp_tworules.py kiw -n3
+time python -O -W ignore exp_tworules.py wik -n3
+time python -O -W ignore exp_tworules.py sla -n3
+time python -O -W ignore exp_tworules.py epi -n3
+time python -O -W ignore exp_tworules.py kiw -n3

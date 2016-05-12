7f694ce461813e5ea3013f102b58734c05cd7dc7
Géraud Le Falher <daureg@gmail.com>
2016-05-12 21:49:42 +0200

    run proper exps :(

--- veverica/runexp.sh
+++ veverica/runexp.sh
@@ -1,4 +1,5 @@
-time python -O -W ignore exp_tworules.py wik -n2
-time python -O -W ignore exp_tworules.py sla -n2
-time python -O -W ignore exp_tworules.py epi -n2
-time python -O -W ignore exp_tworules.py kiw -n2
+time python -O -W ignore lprop_matrix.py wik -n2
+time python -O -W ignore lprop_matrix.py sla -n2
+time python -O -W ignore lprop_matrix.py epi -n2
+time python -O -W ignore lprop_matrix.py kiw -n2
+time python -O -W ignore lprop_matrix.py aut -n2

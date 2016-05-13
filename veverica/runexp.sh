60954d4186ddbb40c599b72f6710e4992a8d4cda
Géraud Le Falher <daureg@gmail.com>
2016-05-13 15:18:23 +0200

    launching final exps

--- veverica/runexp.sh
+++ veverica/runexp.sh
@@ -1,5 +1,5 @@
-time python -O -W ignore lprop_matrix.py wik -n2
-time python -O -W ignore lprop_matrix.py sla -n2
-time python -O -W ignore lprop_matrix.py epi -n2
-time python -O -W ignore lprop_matrix.py kiw -n2
-time python -O -W ignore lprop_matrix.py aut -n2
+time python -O -W ignore icml_exp.py aut -n3
+time python -O -W ignore icml_exp.py wik -n3
+time python -O -W ignore icml_exp.py sla -n3
+time python -O -W ignore icml_exp.py epi -n3
+time python -O -W ignore icml_exp.py kiw -n3

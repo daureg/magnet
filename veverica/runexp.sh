846e1b811cf623e9daef206547be4bc63d8e7d22
Géraud Le Falher <daureg@gmail.com>
2016-09-19 13:37:38 +0200

    re run optim experience

--- veverica/runexp.sh
+++ veverica/runexp.sh
@@ -1,5 +1,5 @@
-# time python -O -W ignore compare_optim_lprop.py aut -n3 &
+time python -O -W ignore compare_optim_lprop.py aut -n3
 time python -O -W ignore compare_optim_lprop.py wik -n3
 time python -O -W ignore compare_optim_lprop.py sla -n3
 time python -O -W ignore compare_optim_lprop.py epi -n3
-# time python -O -W ignore compare_optim_lprop.py kiw -n3 &
+time python -O -W ignore compare_optim_lprop.py kiw -n3

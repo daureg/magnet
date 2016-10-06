d22a3514edb0527654d9d38effd4968a09a1ff75
Géraud Le Falher <daureg@gmail.com>
2016-10-06 16:56:41 +0200

    run at missing 5 and 7 levels

--- veverica/icml_exp.py
+++ veverica/icml_exp.py
@@ -113,6 +113,7 @@ if __name__ == '__main__':
           {'sampling': lambda d: int(ceil(5*log(d)))}]
 
     batch = [{'batch': v} for v in [.03, .09, .15, .20, .25,]]
+    batch = [{'batch': v} for v in [.05, .07]]
 
     fres = [[] for _ in range(35)]
     for r, params in enumerate(cs if args.active else batch):

6ddc64aaa76f2a4ead3c369ef265ac94a7aab6c6
Géraud Le Falher <daureg@gmail.com>
2017-02-15 11:35:26 +0100

    implement the Weighted Tree Algorithm

---
+++ veverica/test_wta.py
@@ -0,0 +1,117 @@
+# vim: set fileencoding=utf-8
+from __future__ import division
+from wta import (LineNode, dfs_order, propagate_revelead_signs,
+                 remove_duplicates, convert_to_line, linearize_tree,
+                 predict_signs)
+from grid_stretch import add_edge
+import random
+import pytest
+
+
+def test_propagate_revelead_signs():
+    n = 9
+    nodes = [LineNode(id_=i) for i in range(n)]
+    for i, ln in enumerate(nodes):
+        if i > 0:
+            ln.left = nodes[i-1]
+        if i < n-1:
+            ln.right = nodes[i+1]
+    nodes[2].sign = 1
+    nodes[6].sign = -1
+    weights = [1, 1/2, 1/2, 3, 1, 1/2, 1/2, 1/2]
+    ew = {(u, v): w for u, v, w in zip(range(n), range(1, n), weights)}
+    propagate_revelead_signs(nodes, ew)
+    answer = [(u.sign if u.sign is not None else u.pred_sign, u.source, u.smallest_dst)
+              for u in nodes]
+    correct_anwser = [(1, 2, 3),
+                      (1, 2, 2),
+                      (1, 2, 0),
+                      (1, 2, 2),
+                      (1, 2, 2+1/3),
+                      (-1, 6, 2),
+                      (-1, 6, 0),
+                      (-1, 6, 2),
+                      (-1, 6, 4),
+                      ]
+    assert answer == correct_anwser
+    answer = propagate_revelead_signs(nodes, ew, return_prediction=True)
+    correct_anwser = {0: 1, 1: 1, 3: 1, 4: 1, 5: -1, 7: -1, 8: -1}
+    assert answer == correct_anwser
+
+
+@pytest.fixture
+def tree():
+    ew = {(1, 5): 4,
+          (1, 6): 1,
+          (1, 8): 1/2,
+          (0, 6): 3,
+          (3, 6): 3,
+          (6, 7): 1/2,
+          (2, 8): 3,
+          (4, 8): 1/2}
+    tree_adj = {}
+    for u, v in ew.keys():
+        add_edge(tree_adj, u, v)
+    return tree_adj, ew
+
+
+def test_dfs_order(tree):
+    tree_adj, ew = tree
+    correct_anwser = [1, 5, 1, 6, 0, 6, 3, 6, 7, 6, 1, 8, 2, 8, 4]
+    answer = dfs_order(tree_adj, ew, 1)
+    assert answer == correct_anwser
+
+
+def test_remove_duplicates(tree):
+    tree_adj, ew = tree
+    initial_order = [1, 5, 1, 6, 0, 6, 3, 6, 7, 6, 1, 8, 2, 8, 4]
+    correct_anwser = [(1, 4), (5, 1), (6, 3), (0, 3), (3, 1/2),
+                      (7, 1/2), (8, 3), (2, 1/2), (4, None)]
+    answer = remove_duplicates(initial_order, ew)
+    assert answer == correct_anwser
+
+
+def test_convert_line():
+    node_and_weight = [(1, 4), (5, 1), (6, 3), (0, 3), (3, 1/2),
+                       (7, 1/2), (8, 3), (2, 1/2), (4, None)]
+    nodes = {u: LineNode(id_=u) for u, _ in node_and_weight}
+    nodes[1].right = nodes[5]
+    nodes[5].right = nodes[6]
+    nodes[6].right = nodes[0]
+    nodes[0].right = nodes[3]
+    nodes[3].right = nodes[7]
+    nodes[7].right = nodes[8]
+    nodes[8].right = nodes[2]
+    nodes[2].right = nodes[4]
+    nodes[5].left = nodes[1]
+    nodes[6].left = nodes[5]
+    nodes[0].left = nodes[6]
+    nodes[3].left = nodes[0]
+    nodes[7].left = nodes[3]
+    nodes[8].left = nodes[7]
+    nodes[2].left = nodes[8]
+    nodes[4].left = nodes[2]
+    correct_anwser = ([nodes[u] for u, _ in node_and_weight],
+                      {(1, 5): 4, (5, 6): 1, (0, 6): 3, (0, 3): 3,
+                       (3, 7): 1/2, (7, 8): 1/2, (2, 8): 3, (2, 4): 1/2})
+    answer = convert_to_line(node_and_weight)
+    assert answer == correct_anwser
+
+
+def test_predict_signs(tree):
+    tree_adj, ew = tree
+    root = 1
+    signs = {0: 1, 1: 1, 2: 1, 3: 1, 4: 1, 5: -1, 6: -1, 7: -1, 8: -1}
+    n = len(signs)
+    nodes_line, edge_weight = convert_to_line(linearize_tree(tree_adj, ew, root))
+    for i in range(10):
+        training_set = {u: signs[u] for u in [random.randint(0, n-1), random.randint(0, n-1)]}
+        predict_signs(nodes_line, edge_weight, training_set)
+    training_set = {u: signs[u] for u in [2, 6]}
+    answer = predict_signs(nodes_line, edge_weight, training_set)
+    correct_anwser = {0: -1, 1: -1, 3: -1, 4: 1, 5: -1, 7: 1, 8: 1}
+    assert answer == correct_anwser
+
+
+if __name__ == "__main__":
+    test_convert_line()
---
+++ veverica/wta.py
@@ -0,0 +1,174 @@
+# vim: set fileencoding=utf-8
+"""
+Implement the WTA node binary classification algorithm.
+
+from Cesa-Bianchi, N., Gentile, C., Vitale, F. & Zappella, G.
+Random Spanning Trees and the Prediction of Weighted Graphs.
+J. Mach. Learn. Res. 14, 1251–1284 (2013).
+http://www.jmlr.org/papers/v14/cesa-bianchi13a.html .
+"""
+from __future__ import division
+from collections import defaultdict
+from attr.validators import instance_of
+import attr
+
+
+def is_binary(instance, attribute, x):
+    if x not in {-1, 1, None}:
+        msg = 'Expecting a binary value for {} instead of {}'
+        raise ValueError(msg.format(attribute.name, x))
+
+
+def is_line_node(instance, attribute, x):
+    if not isinstance(x, LineNode) and x is not None:
+        msg = 'Expecting a LineNode for {} instead of {}'
+        raise ValueError(msg.format(attribute.name, type(x)))
+
+
+@attr.s(slots=True)
+class LineNode(object):
+    id_ = attr.ib(validator=instance_of(int))
+    left = attr.ib(default=None, cmp=False, repr=False, validator=is_line_node)
+    right = attr.ib(default=None, cmp=False, repr=False, validator=is_line_node)
+    sign = attr.ib(default=None, validator=is_binary)
+    smallest_dst = attr.ib(default=1e9, validator=instance_of(float))
+    source = attr.ib(default=-1, validator=instance_of(int))
+    pred_sign = attr.ib(default=None, validator=is_binary)
+
+
+@attr.s(slots=True)
+class Propagated(object):
+    id_ = attr.ib(validator=instance_of(int))
+    sign = attr.ib(validator=is_binary)
+    origin = attr.ib(validator=instance_of(int))
+    going_right = attr.ib(validator=instance_of(bool))
+    pos = attr.ib(cmp=False, hash=False, validator=instance_of(int))
+    dst = attr.ib(cmp=False, hash=False, convert=float, validator=instance_of(float))
+
+
+def propagate_revelead_signs(nodes_line, edge_weight, return_prediction=True):
+    propas = set()
+    for i, u in enumerate(nodes_line):
+        if u.sign is not None:
+            j = len(propas)//2
+            propas.add(Propagated(id_=2*j, pos=i, dst=0, sign=u.sign, going_right=False, origin=i))
+            propas.add(Propagated(id_=2*j+1, pos=i, dst=0, sign=u.sign, going_right=True, origin=i))
+            u.source = i
+            u.smallest_dst = 0
+    n = len(nodes_line)
+    nb_iter = 0
+    while propas and nb_iter < 2*n:
+        to_remove = set()
+        for p in iter(propas):
+            nei_idx = p.pos + (1 if p.going_right else -1)
+            if not 0 <= nei_idx < n:
+                to_remove.add(p)
+                continue
+            nei = nodes_line[nei_idx]
+            u, v = nei.id_, nodes_line[p.pos].id_
+            w = edge_weight[(u, v) if u < v else (v, u)]
+            dst = p.dst + 1/w
+            if nei.sign is not None or nei.smallest_dst < dst + 1e-9:
+                to_remove.add(p)
+                continue
+            nei.pred_sign = p.sign
+            nei.smallest_dst = dst
+            nei.source = p.origin
+            p.pos = nei_idx
+            p.dst += 1/w
+        propas.difference_update(to_remove)
+        nb_iter += 1
+    if return_prediction:
+        return {u.id_: u.pred_sign for u in nodes_line if u.pred_sign is not None}
+
+
+def dfs_order(tree_adj, edge_weight, root):
+    assert isinstance(tree_adj, dict)
+    stack = [root, ]
+    status = defaultdict(bool)
+    res = []
+    last_node = None
+    while stack:
+        v = stack.pop()
+        if v < 0:
+            v = -(v+100)
+        discovered = status[v]
+        res.append(v)
+        if v == last_node:
+            return res
+        if not discovered:
+            status[v] = True
+            if v not in tree_adj or len(tree_adj[v]) == 1:
+                continue
+            for w in sorted(tree_adj[v], reverse=True):
+                discovered = status[w]
+                if len(status) == len(tree_adj) and last_node is None:
+                    last_node = max(tree_adj[v])
+                if not discovered:
+                    stack.append(-(v+100))
+                    stack.append(w)
+
+
+def remove_duplicates(line, ew):
+    seen = set((line[0], ))
+    i, n = 0, len(line)
+    res = []
+    while i < n:
+        u = line[i]
+        weights = set()
+        for j, v in enumerate(line[i+1:]):
+            prev_u = line[i+1+j-1]
+            weights.add(ew[(prev_u, v)] if prev_u < v else ew[(v, prev_u)])
+            if v not in seen:
+                break
+        w = None if not weights else min(weights)
+        seen.add(v)
+        res.append((u, w))
+        i += j+1
+    return res
+
+
+def linearize_tree(tree_adj, edge_weight, root):
+    ordered_node = dfs_order(tree_adj, edge_weight, root)
+    return remove_duplicates(ordered_node, edge_weight)
+
+
+def convert_to_line(node_and_weight):
+    nodes = []
+    line_weight = {}
+    for (u, w), (v, _) in zip(node_and_weight, node_and_weight[1:]):
+        nodes.append(LineNode(id_=u))
+        line_weight[(u, v) if u < v else (v, u)] = w
+    nodes.append(LineNode(id_=node_and_weight[-1][0]))
+    for u, v in zip(nodes, nodes[1:]):
+        u.right = v
+        v.left = u
+    return nodes, line_weight
+
+
+def predict_signs(nodes_line, edge_weight, training_set):
+    for n in nodes_line:
+        n.pred_sign = None
+        n.sign = training_set.get(n.id_, None)
+        n.smallest_dst = 1e9
+        n.source = -1
+    return propagate_revelead_signs(nodes_line, edge_weight)
+
+
+if __name__ == "__main__":
+    import random
+    from shazoo_exps import make_graph
+    from timeit import default_timer as clock
+    n = 1000
+    random.seed(123)
+    tree_adj, ew, signs, _ = make_graph(20000)
+    timings = []
+    nodes_line, edge_weight = convert_to_line(linearize_tree(tree_adj, ew, 0))
+    nrep = 20
+    for _ in range(nrep):
+        training_set = {u: signs[u] for u in random.sample(list(range(n)), int(.1*n))}
+        start = clock()
+        predict_signs(nodes_line, edge_weight, training_set)
+        timings.append(clock() - start)
+    print('\t'.join(('{:.3g}'.format(t) for t in timings)))
+    print(sum(timings[1:])/(nrep-1))

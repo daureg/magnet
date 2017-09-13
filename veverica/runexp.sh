daf80d5828d859f8c493c43684e2c7e960df20ca
Géraud Le Falher <daureg@gmail.com>
2017-09-13 17:35:05 +0200

    rerun temporal exps with the correct keyword

--- veverica/runexp.sh
+++ veverica/runexp.sh
@@ -1,6 +1,6 @@
 # time python -O -W ignore icml_exp.py -n3 wik
 # time python -O -W ignore icml_exp.py -n3 aut
-time python -O -W ignore icml_exp.py -n3 adv
+# time python -O -W ignore icml_exp.py -n3 adv
 # time python -O -W ignore icml_exp.py -n3 sla
 # time python -O -W ignore icml_exp.py -n3 epi
 # time python -O -W ignore icml_exp.py -n3 kiw
@@ -10,7 +10,7 @@ time python -O -W ignore icml_exp.py -n3 adv
 # time python -O -W ignore time_triads.py -n3 sla
 # time python -O -W ignore time_triads.py -n3 epi
 # time python -O -W ignore time_triads.py -n3 kiw
-# time python -O -W ignore time_triads.py -n3 wik_ts
-# time python -O -W ignore icml_exp.py -n3 wik_ts
-# time python -O -W ignore time_triads.py -n3 epi_ts
-# time python -O -W ignore icml_exp.py -n3 epi_ts
+time python -O -W ignore time_triads.py -n3 wik_ts
+time python -O -W ignore icml_exp.py -n3 wik_ts
+time python -O -W ignore time_triads.py -n3 epi_ts
+time python -O -W ignore icml_exp.py -n3 epi_ts

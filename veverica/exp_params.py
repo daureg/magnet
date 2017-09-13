64f9dcf6810979855911293ed40dca969dc974ad
Géraud Le Falher <daureg@gmail.com>
2017-09-13 16:49:37 +0200

    advogato needs more lprop iterations

--- veverica/exp_params.py
+++ veverica/exp_params.py
@@ -1,3 +1,3 @@
 batch = [{'batch': v} for v in [.05, .1, .15, .2, .25, .5, .9]]
-diameters = {'aut': 22, 'wik': 16, 'wik_ts': 16, 'sla': 32, 'epi': 38, 'adv': 9, 'epi_ts': 38,
+diameters = {'aut': 22, 'wik': 16, 'wik_ts': 16, 'sla': 32, 'epi': 38, 'adv': 9*2, 'epi_ts': 38,
              'kiw': 30}
--- veverica/runexp.sh
+++ veverica/runexp.sh
@@ -1,9 +1,9 @@
 # time python -O -W ignore icml_exp.py -n3 wik
 # time python -O -W ignore icml_exp.py -n3 aut
-# time python -O -W ignore icml_exp.py -n3 adv
+time python -O -W ignore icml_exp.py -n3 adv
 # time python -O -W ignore icml_exp.py -n3 sla
-time python -O -W ignore icml_exp.py -n3 epi
-time python -O -W ignore icml_exp.py -n3 kiw
+# time python -O -W ignore icml_exp.py -n3 epi
+# time python -O -W ignore icml_exp.py -n3 kiw
 # time python -O -W ignore time_triads.py -n3 wik
 # time python -O -W ignore time_triads.py -n3 aut
 # time python -O -W ignore time_triads.py -n3 adv
@@ -11,6 +11,6 @@ time python -O -W ignore icml_exp.py -n3 kiw
 # time python -O -W ignore time_triads.py -n3 epi
 # time python -O -W ignore time_triads.py -n3 kiw
 # time python -O -W ignore time_triads.py -n3 wik_ts
-# time python -O -W ignore time_triads.py -n3 epi_ts
 # time python -O -W ignore icml_exp.py -n3 wik_ts
-time python -O -W ignore icml_exp.py -n3 epi_ts
+# time python -O -W ignore time_triads.py -n3 epi_ts
+# time python -O -W ignore icml_exp.py -n3 epi_ts
